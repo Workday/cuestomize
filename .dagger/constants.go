76a6c46ab39d892183d11cd4c82da49badb1452e
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2025-08-25 11:40:34 +0100

    chore: update Go to 1.25

--- .dagger/constants.go
+++ .dagger/constants.go
@@ -3,7 +3,7 @@ package main
 import "dagger/cuestomize/internal/dagger"
 
 const (
-	GolangImage           = "golang:1.24"
+	GolangImage           = "golang:1.25"
 	RegistryImage         = "registry:2"
 	DistrolessStaticImage = "gcr.io/distroless/static:latest"
 	KustomizeImage        = "registry.k8s.io/kustomize/kustomize:v5.7.1"
