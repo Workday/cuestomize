0ce24e817acaafd886299172da7540a2d9caa410
Lorenzo Felletti <lorenzo97.felletti@gmail.com>
2025-11-28 15:08:44 +0000

    refactor: update error handling and detailer usage in context management

--- internal/pkg/cuestomize/function.go
+++ internal/pkg/cuestomize/function.go
@@ -22,6 +22,9 @@ type KRMFunction = func([]*kyaml.RNode) ([]*kyaml.RNode, error)
 // * resourcesPath: path to the directory containing the CUE resources (nil to use the default)
 func newCuestomizeFunctionWithPath(ctx context.Context, config *api.KRMInput, resourcesPath *string) KRMFunction {
 	return func(items []*kyaml.RNode) ([]*kyaml.RNode, error) {
+		detailer := cuerrors.NewDefaultDetailer(*resourcesPath)
+		ctx = cuerrors.NewContext(ctx, detailer)
+
 		return cuestomize.Cuestomize(ctx, items, config, *resourcesPath)
 	}
 }
